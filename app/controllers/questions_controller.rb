e226d5aa4dd160a7fcac427fed281c3eb8de8fc5
frisk <friskslalom6@Randalls-MacBook-Pro.local>
2013-05-06 14:40:29 -0700

    removed code in index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,18 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.all
-    counter = Question.count
-    qid = rand(1..counter)
-    if qid == 0
-      qid = 1
-    end
-    new_question = Question.find(qid)
-    answers = new_question.answers.all
-    data = { :questions => new_question, :answers => answers }
-    respond_to do |format|
-      format.html
-      format.json { render json: data }
-    end
+    # @questions = Question.all
+    # counter = Question.count
+    # qid = rand(1..counter)
+    # if qid == 0
+    #   qid = 1
+    # end
+    # new_question = Question.find(qid)
+    # answers = new_question.answers.all
+    # data = { :questions => new_question, :answers => answers }
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: data }
+    # end
   end
 
   def new
